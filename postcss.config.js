707467ed86ee81c1bb399f7aaa12486ab72a07c9
Johan Brook <johan@lookback.io>
2019-09-26 11:24:29 +0200

    Use PurgeCSS in prod

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,18 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './src/site/**/*.njk',
+    './src/site/**/*.md',
+    './src/site/**/*.html',
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
+const minify = require('cssnano')({
+  preset: 'default',
+});
+
 module.exports = (ctx) => ({
   plugins: [
     require('postcss-import')(),
@@ -5,6 +20,7 @@ module.exports = (ctx) => ({
     require('postcss-color-function')(),
     require('postcss-hexrgba')(),
     require('autoprefixer')(),
-    ...(ctx.env === 'production' ? require('cssnano')() : []),
+    ...(ctx.env === 'production' ? [purgecss] : []),
+    ...(ctx.env === 'production' ? [minify] : []),
   ],
 });
