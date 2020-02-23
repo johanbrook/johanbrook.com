2c4bfd4ef1491cc9e8165eaa88868b7d9e1cec59
Johan Brook <johan@lookback.io>
2020-02-23 19:02:32 +0100

    Remove PurgeCSS for now
    Because it messed up some styling.

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,3 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [
-    './src/site/**/*.njk',
-    './src/site/**/*.md',
-    './src/site/**/*.html',
-  ],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
 const minify = require('cssnano')({
   preset: 'default',
 });
@@ -22,7 +11,6 @@ module.exports = (ctx) => ({
     require('postcss-color-function')(),
     require('postcss-hexrgba')(),
     require('autoprefixer')(),
-    ...(env === 'production' ? [purgecss] : []),
     ...(env === 'production' ? [minify] : []),
   ],
 });
