33a2b3d56b6c72daedf1ccb614fa4b8c33b84704
Johan Brook <johan@johanbrook.com>
2021-12-26 21:58:14 +0100

    Inline JS script

--- _config.ts
+++ _config.ts
@@ -2,6 +2,7 @@ import lume from 'lume/mod.ts';
 import postcss from 'lume/plugins/postcss.ts';
 import codeHighlight from 'lume/plugins/code_highlight.ts';
 import date from 'lume/plugins/date.ts';
+import inline from 'lume/plugins/inline.ts';
 import postcssNested from 'https://esm.sh/postcss-nested@5';
 import postcssColor from 'https://esm.sh/postcss-color-function@4';
 import postcssHexRgba from 'https://esm.sh/postcss-hexrgba@2';
@@ -18,6 +19,7 @@ site
   .copy('public', '.')
   // Plugins
   .use(codeHighlight())
+  .use(inline())
   .use(
     postcss({
       sourceMap: true,
@@ -27,11 +29,9 @@ site
   )
   .use(date())
   // Helpers
-  .filter('substr', (str, len) => str.substr(0, len))
-  .filter('moreThan', (num, count) => num > count)
+  .filter('substr', (str: string, len: number) => str.substr(0, len))
+  .filter('moreThan', (num: number, count: number) => num > count)
   // Don't the entire site rebuild when --watching or --serving if .css files change
   .scopedUpdates((path) => path.endsWith('.css'));
-
-// site.copy('_data/meta.json', 'meta.json');
 
 export default site;
