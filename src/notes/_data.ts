70fdff07b786e54a4b312d92072d784ba8cae5be
Johan Brook <johan@lookback.io>
2023-07-17 22:34:58 +0200

    Fix URLs for notes

--- src/notes/_data.ts
+++ src/notes/_data.ts
@@ -1,5 +1,8 @@
 import { format } from 'lume/deps/date.ts';
+import { urlForNote } from '../_includes/permalinks.ts';
 
 export const permalink = function (this: { date: Date }) {
 	return `/mind#${format(this.date, 'yyyyMMddHHmm', {})}`;
 };
+
+export const url = urlForNote;
