c3b94610d2b797a0d3dc727574fe5f651599b3fe
Johan Brook <johan@lookback.io>
2024-01-09 09:14:47 +0100

    Remove syntax highlighting and minification
    Only adds complexity and mental weight.

--- _config.ts
+++ _config.ts
@@ -4,11 +4,8 @@ import postcss from 'lume/plugins/postcss.ts';
 import esbuild from 'lume/plugins/esbuild.ts';
 import date from 'lume/plugins/date.ts';
 import inline from 'lume/plugins/inline.ts';
-import minifyHTML from 'lume/plugins/minify_html.ts';
-import { minifier } from './deps.ts';
 import { readingTime } from './src/_lume-plugins/reading-time.ts';
 import { extractExcerpt } from './src/_lume-plugins/excerpts.ts';
-import { loadLanguages, prismMarkdown } from './src/_lume-plugins/prism.ts';
 import { typeset } from './src/_lume-plugins/typeset.ts';
 import sourceMaps from 'lume/plugins/source_maps.ts';
 import { idOf } from './src/_includes/permalinks.ts';
@@ -16,21 +13,12 @@ import { idOf } from './src/_includes/permalinks.ts';
 const DEST = 'build';
 const MINIFY = Deno.env.get('ENV') == 'production';
 
-loadLanguages();
-
 const site = lume(
 	{
 		src: 'src',
 		dest: DEST,
 		location: new URL('https://johan.im'), // Ignored in dev
-	},
-	{
-		markdown: {
-			options: {
-				highlight: prismMarkdown,
-			},
-		},
-	},
+	}
 );
 
 site
@@ -106,20 +94,5 @@ site
 	.data('templateEngine', 'njk,md', '/notes')
 	// Don't the entire site rebuild when --watching or --serving if .css files change
 	.scopedUpdates((path) => path.endsWith('.css'));
-
-if (MINIFY) {
-	site.addEventListener('afterBuild', () => {
-		console.log('Minifying CSS…');
-		minifyCss();
-	});
-
-	site.use(minifyHTML());
-}
-
-const minifyCss = () => {
-	const css = Deno.readTextFileSync(`./${DEST}/johan.css`);
-	const minified = minifier.minify('css', css);
-	Deno.writeTextFileSync(`./${DEST}/johan.css`, minified);
-};
 
 export default site;
--- deps.ts
+++ deps.ts
@@ -1,3 +1,1 @@
-export * as minifier from 'https://deno.land/x/minifier@v1.1.1/mod.ts';
-export type { Endpoints } from 'https://cdn.skypack.dev/@octokit/types@^6?dts';
 export * as prism from 'https://cdn.skypack.dev/prismjs@^1?dts';
